96655af1c2a751d65427b69a65c94a408c8f1016
Ian Chan <ichan@csusm.edu>
2016-07-21 16:51:31 -0700

    Remove the CSUSM-specific adjustment for SBSB.

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if ($_GET["userid"] != "" && strlen($_GET["userid"]) < 15) {
     $phone1 = $user->xpath('/userdata/user/contact_info/phones/phone[@preferred="true"]');
     $dept1 = $user->xpath('/userdata/user/user_statistics/user_statistic[@segment_type="External"]');
     if (count($dept1) == 0){
-          $dept1 = $user->xpath('/userdata/user/user_statistics/user_statistic');
+      $dept1 = $user->xpath('/userdata/user/user_statistics/user_statistic');
     }
     echo '<?xml version="1.0" encoding="utf-8"?>'. "\n";
     $xml_output = "";
@@ -52,11 +52,7 @@ if ($_GET["userid"] != "" && strlen($_GET["userid"]) < 15) {
     $xml_output .= "\t\t<email>" . $email1[0]->email_address . "</email>\n";
     $xml_output .= "\t\t<phone>" . $phone1[0]->phone_number . "</phone>\n";
 
-    if (strstr($useraddressfull[0]->line1, "Social and Behavioral Science Building")){
-      $userstaddress1 = str_replace("Social and Behavioral Science Building", "SBSB", $useraddressfull[0]->line1);
-    } else {
-      $userstaddress1 = $useraddressfull[0]->line1;
-    }
+    $userstaddress1 = $useraddressfull[0]->line1;
     if (strlen($userstaddress1) > 40) {
       $userstaddressSplit = str_split($userstaddress1, strrpos(substr($userstaddress1, 0, 39), ' '));
       $userstaddress1 = $userstaddressSplit[0];
@@ -65,8 +61,8 @@ if ($_GET["userid"] != "" && strlen($_GET["userid"]) < 15) {
     if ($useraddressfull[0]->line2 != ""){
       $userstaddress2 .= ", " . $useraddressfull[0]->line2;
     }
-    $xml_output .= "\t\t<streetaddr1>" . $userstaddress1 . "</streetaddr1>\n";
-    $xml_output .= "\t\t<streetaddr2>" . $userstaddress2 . "</streetaddr2>\n";
+    $xml_output .= "\t\t<streetaddr1>" . htmlspecialchars($userstaddress1) . "</streetaddr1>\n";
+    $xml_output .= "\t\t<streetaddr2>" . htmlspecialchars($userstaddress2) . "</streetaddr2>\n";
     $xml_output .= "\t\t<city>" . $useraddressfull[0]->city . "</city>\n";
     if (strlen($useraddressfull[0]->state_province) > 2){
       $stateabbr = convert_state_to_abbreviation($useraddressfull[0]->state_province);
